ab549d495963c8da0a68c5d173413c2646a5b79c
udaymashetti <52440007+udaymashetti@users.noreply.github.com>
2019-11-25 11:16:27 +0530

    Add files via upload

---
+++ Zuul/src/main/java/com/demo/Zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.demo.Zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EnableOAuth2Sso
+
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
